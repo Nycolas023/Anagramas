c7b069d79123f7494780fc7f0f5f8ebc98617012
Nycolas F <79328874+Nycolas023@users.noreply.github.com>
2025-04-04 02:24:47 -0300

    Adicionar arquivos de projeto.

---
+++ anagramas/Program.cs
@@ -0,0 +1,37 @@
+﻿//Duas palavras são anagramas se e somente se possuem as mesmas letras na mesma frequencia
+//Ex:
+//amor - orma-- true
+//aac - aca-- true
+//baa - bba-- false
+//qwert - qwerty - false
+
+
+//Desenvolva uma algoritmo que determina se duas palavras são anagramas utilizando apenas funcinalidades basicas da linguagem como loops e estrutura de dados como listas, objetos, diagramas, etc...
+
+Console.WriteLine("Escrava a sua primeira palavra anagrama: ");
+string ? palavra1 = Console.ReadLine();
+
+Console.WriteLine("Escrava a sua segunda palavra anagrama: ");
+string ? palavra2 = Console.ReadLine();
+
+int index1 = palavra1.Length;
+int index2 = palavra2.Length;
+
+string[] a = new string[index1];
+string[] b = new string[index2];
+
+try 
+{
+    if (a.Length == b.Length)
+    {
+      
+    }
+    else 
+    {
+        throw new Exception("Ambas palavras não possuem a mesma quantia de letras");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("\n" + ex.ToString());
+}
